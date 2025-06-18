ad13466a644cec77abf8d875ec5cbd1e4260ddfd
May <k.bureechai@gmail.com>
2025-06-18 08:23:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+const convertBtn = document.getElementById("convertBtn")
+const input = document.getElementById("unitInput")
+const resultLength = document.getElementById("meterAndFeet")
+const resultVolume = document.getElementById("litersAndGallons")
+const resultMass = document.getElementById("kgAndPounds")
+
+function convertUnit() {
+    
+    const value = parseFloat(input.value);
+    if (isNaN(value)) {
+       resultLength.textContent = "Please enter a valid number.";
+        resultVolume.textContent = "Please enter a valid number.";
+        resultMass.textContent = "Please enter a valid number.";
+        return;
+
+    }
+    
+    const numFeet = value * 3.281;
+    const numMeter = value * 0.3048;
+    resultLength.textContent = `${value} meters = ${numFeet.toFixed(3)} feet | ${value} feet = ${numMeter.toFixed(3)} meters`
+   
+   const numLiters = value * 0.264;
+   const numGallons = value * 3.78541;
+   resultVolume.textContent = `${value} liters = ${numLiters.toFixed(3)} gallons | ${value} gallons = ${numGallons.toFixed(3)}`
+   
+   const numKg = value * 2.204;
+   const numPounds = value * 0.453592;
+   resultMass.textContent = `${value} kilos = ${numKg.toFixed(3)} pounds | ${value} pounds = ${numPounds.toFixed(3)}`
+}
+
+convertBtn.addEventListener("click", convertUnit)
